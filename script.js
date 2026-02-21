f2bb762cf211a69e86b42f6826af92bb7f00ccb9
AfsaraSilviAmi <afsarasilvi44@gmail.com>
2026-02-22 00:19:02 +0600

    can change the text for not applicable

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-let InterBtn = document.querySelectorAll(".interv-btn");
+let interBtn = document.querySelectorAll(".interv-btn");
+let rejectBtn = document.querySelectorAll(".reject-btn");
 
-for(let btn of InterBtn){
-    btn.addEventListener('click', function(){
-        console.log("click");
+for(let btn of interBtn){
+    btn.addEventListener('click', function(e){
+       let card = e.target.closest(".card");
+       let changeInter = card.querySelector(".change-job-text");
+       changeInter.innerText = "Interview";
+       changeInter.classList.add("bg-green-100", "text-[#10B981]", "text-[17px]", "border", "border-[#10B981]", "rounded-xl");
+
+       
+    })
+}
+
+
+for(let btn of rejectBtn){
+    btn.addEventListener('click', function(e){
+       let card = e.target.closest(".card");
+       let changeRej = card.querySelector(".change-job-text");
+       changeRej.innerText = "Rejected";
+       changeRej.classList.add("bg-red-100", "text-[#EF4444]", "text-[17px]", "border", "border-[#EF4444]", "rounded-xl");
+
+       
     })
 }
